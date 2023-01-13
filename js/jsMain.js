979e520dc92c48355cbac123bc8958f1c7f04073
mboschini <boschini.mariano@gmail.com>
2023-01-13 07:12:11 -0300

    demo gallery view

--- js/jsMain.js
+++ js/jsMain.js
@@ -1,10 +1,21 @@
-const navButtons = document.getElementsByClassName("NavButton")
+var buttonprod = document.getElementById("prodcutosbtn");
+var buttonabout = document.getElementById("aboutbtn");
+var buttoncontact = document.getElementById("contactobtn");
 
-function NavButtonClick(buttonText)
-{
-    Array.from(navButtons).forEach(navButtons =>    
-    {
-        if(buttonText==navButtons.textContent)
-            console.log(navButtons.textContent)
-    });
-}
+buttonprod.addEventListener('click', function (event) {
+    event.preventDefault();
+    var section1 = document.getElementById("featured-products");
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+buttonabout.addEventListener('click', function(event) {
+    event.preventDefault();
+    var section2 = document.getElementById("about");
+    section2.scrollIntoView({ behavior: 'smooth' });
+});
+
+buttoncontact.addEventListener('click', function (event) {
+    event.preventDefault();
+    var section3 = document.getElementById("contactanos");
+    section3.scrollIntoView({ behavior: 'smooth' });
+});
